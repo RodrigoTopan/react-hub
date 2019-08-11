5ece8528145a7ce130e66e2b06a207d5d00f8c96
Rodrigo Topan <rodrigo.topan.ti@gmail.com>
2019-08-11 18:19:30 -0300

    Basic Structure + Eslint Config + Routes

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+function Main() {
+    return (
+        <Title error>
+            Olá Mundo
+            <small>Olá mundo pequeno</small>
+        </Title>
+    );
+}
+
+export default Main;
